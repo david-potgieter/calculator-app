c96530768b75d5526991ebfa31ff45207534a85d
David Potgieter <david@weswingcats.com>
2023-08-09 13:40:54 +0700

    added and split svg icons

---
+++ src/icons/equal.ts
@@ -0,0 +1,16 @@
+export const equalHTML = `
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    class="keypad-icon keypad-icon_operator"
+    viewBox="0 0 128 128">
+        <g id="_x36_">
+            <path style="fill:currentColor;" d="M125.61,39.284H2.39c-1.32,0-2.39-1.07-2.39-2.39v-9.787c0-1.32,1.07-2.39,2.39-2.39h123.22
+            c1.32,0,2.39,1.07,2.39,2.39v9.787C128,38.214,126.93,39.284,125.61,39.284z M128,100.802v-9.787c0-1.32-1.07-2.39-2.39-2.39H2.39
+            c-1.32,0-2.39,1.07-2.39,2.39v9.787c0,1.32,1.07,2.39,2.39,2.39h123.22C126.93,103.192,128,102.122,128,100.802z" />
+        </g>
+</svg>
+<span class="visually-hidden">
+    Equals
+</span>
+`
---
+++ src/icons/icons.ts
@@ -0,0 +1,19 @@
+import { addHTML } from './add'
+import { divideHTML } from './divide'
+import { equalHTML } from './equal'
+import { exponentHTML } from './exponent'
+import { moonHTML } from './moon'
+import { multiplyHTML } from './multiply'
+import { subtractHTML } from './subtract'
+import { sunHTML } from './sun'
+
+export {
+  addHTML,
+  divideHTML,
+  equalHTML,
+  exponentHTML,
+  moonHTML,
+  multiplyHTML,
+  subtractHTML,
+  sunHTML,
+}
