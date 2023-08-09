7384d0ffaf34c0e738f6d518fc07e370a3c8011a
David Potgieter <david@weswingcats.com>
2023-08-09 14:14:25 +0700

    split out funcs and consts

---
+++ src/functions/setup-toggle-mode.ts
@@ -0,0 +1,21 @@
+import { MODE } from '../config/consts'
+
+export function setupToggleMode(toggles: NodeListOf<Element>) {
+  toggles.forEach((button) => {
+    button.addEventListener('click', (event: Event) => {
+      const target = event.target as HTMLElement
+      const modeSelected = target?.getAttribute('data-toggle')
+      const isDark = document.body.classList.contains('dark_mode')
+      switch (true) {
+        case modeSelected === MODE.MODE_ON && !isDark:
+          document.body.classList.add('dark_mode')
+          break
+        case modeSelected === MODE.MODE_OFF && isDark:
+          document.body.classList.remove('dark_mode')
+          break
+        default:
+          break
+      }
+    })
+  })
+}
